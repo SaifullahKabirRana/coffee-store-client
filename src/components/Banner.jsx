f1fe583ee18f6715f4e1c44b45bfe0556bac409e
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2024-08-26 01:24:49 +0600

    added banner details

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,7 @@
-
+import icon1 from '../assets/icons/1.png'
+import icon2 from '../assets/icons/2.png'
+import icon3 from '../assets/icons/3.png'
+import icon4 from '../assets/icons/4.png'
 const Banner = () => {
     return (
         <div>
@@ -9,6 +12,33 @@ const Banner = () => {
                     <button className="btn rounded-none btn-sm md:btn-md lg:px-6 text-base md:text-lg lg:text-2xl font-rancho border-none bg-[#E3B577]  mt-4 md:mt-6 lg:mt-8">Learn More</button>
                 </div>
 
+            </div>
+
+            <div className="bg-[#ECEAE3]  md:h-[250px] lg:h-[280px] lg:-mt-28">
+                	<div className='container lg:mx-auto md:mx-8 pb-4 md:pb-0'>
+                        <div className='grid grid-cols-2 md:grid-cols-4 pt-7 md:pt-8 lg:pt-12 pl-10 md:pl-0'>
+                        <div className=''>
+                            <img className='w-[55px] md:w-[70px]' src={icon1} alt="" />
+                             <h2 className='font-rancho text-[25px] md:text-[26px] lg:text-[35px] text-[#331A15] mt-2 md:mt-4'>Awesome Aroma</h2>
+                             <p className='font-raleway text-[11px] md:text-[12px] lg:text-[15px] text-[#1B1A1A] mt-0 md:mt-2 w-[80%] md:w-[80%]'>You will definitely be a fan of the design & aroma of your coffee</p>
+                        </div>
+                        <div className=''>
+                            <img className='w-[55px] md:w-[70px]' src={icon2} alt="" />
+                             <h2 className='font-rancho text-[25px] md:text-[26px] lg:text-[35px] text-[#331A15] mt-2 md:mt-4'>Awesome Aroma</h2>
+                             <p className='font-raleway text-[11px] md:text-[12px] lg:text-[15px] text-[#1B1A1A] mt-0 md:mt-2 w-[80%] md:w-[80%]'>You will definitely be a fan of the design & aroma of your coffee</p>
+                        </div>
+                        <div className='pt-5 md:pt-0'>
+                            <img className='w-[55px] md:w-[70px]' src={icon3} alt="" />
+                             <h2 className='font-rancho text-[25px] md:text-[26px] lg:text-[35px] text-[#331A15] mt-2 md:mt-4'>Awesome Aroma</h2>
+                             <p className='font-raleway text-[11px] md:text-[12px] lg:text-[15px] text-[#1B1A1A] mt-0 md:mt-2 w-[80%] md:w-[80%]'>You will definitely be a fan of the design & aroma of your coffee</p>
+                        </div>
+                        <div className='pt-5 md:pt-0'>
+                            <img className='w-[55px] md:w-[70px]' src={icon4} alt="" />
+                             <h2 className='font-rancho text-[25px] md:text-[26px] lg:text-[35px] text-[#331A15] mt-2 md:mt-4'>Awesome Aroma</h2>
+                             <p className='font-raleway text-[11px] md:text-[12px] lg:text-[15px] text-[#1B1A1A] mt-0 md:mt-2 w-[80%] md:w-[80%]'>You will definitely be a fan of the design & aroma of your coffee</p>
+                        </div>
+                        </div>
+                    </div>
             </div>
         </div>
     );
